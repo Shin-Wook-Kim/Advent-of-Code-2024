ce3225b09b72dcf6b69510d968c3c0cbb1768f5c
Shin-Wook-Kim <110709745+Shin-Wook-Kim@users.noreply.github.com>
2024-12-07 11:37:37 -0600

    finished Day7

---
+++ Day7/Day7Puzzle2.py
@@ -0,0 +1,57 @@
+
+c_equations = []
+
+
+with open("c:/Users/corpt/Documents/Advent-of-Code-2024/Day7/Day7Input1.txt", 'r') as file:
+    file.seek(0)
+    done = False
+    while (not done):
+        line = file.readline()
+        if line == '':
+            done = True
+        else:
+            temp = []
+            s_line1 = line.split(sep=':')
+            temp.append(int(s_line1[0]))
+            s_line2 = s_line1[1].split()
+            for char in s_line2:
+                temp.append(int(char))
+            c_equations.append(temp)
+
+
+
+result = 0
+l = 0
+
+
+
+for equation in c_equations:
+    l = len(equation)
+    test = equation[0]
+    temp = equation[1]
+    if l == 2:
+        if test == temp:
+            result += test
+    else:
+        for i in range(3**(l-2)):
+            temp = equation[1]
+            for j in range(l-2):
+                op = (i // (3 ** j)) % 3
+                if op == 0:
+                    temp = temp + equation[j+2]
+                elif op == 1:
+                    temp = temp * equation[j+2]
+                else:
+                    temp = int(str(temp) + str(equation[j+2]))
+            if test == temp:
+                result += test
+                break
+
+print(result)
+
+
+
+
+
+
+print(result)
